9d4439857848a90c5414fd9880d3cac4d6f86ca8
michaelgathara <mikegtrm@gmail.com>
2024-04-10 00:14:11 -0500

    starting lab5

---
+++ Lab5/task01.cu
@@ -0,0 +1,151 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector_functions.h>
+#include <vector_types.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#define IMAGE_DIM 2048
+
+#define rnd(x) (x * rand() / RAND_MAX)
+#define INF 2e10f
+
+using uchar = unsigned char;
+
+void output_image_file(uchar *image);
+void input_image_file(char *filename, uchar3 *image);
+void checkCUDAError(const char *msg);
+
+__global__ void image_to_grayscale(uchar3 *image, uchar *image_output) {
+    // Add your implementation here
+}
+
+/* Host code */
+
+int main(void) {
+    unsigned int image_size, image_output_size;
+    uchar3 *d_image, *h_image;
+    uchar *d_image_output, *h_image_output;
+    cudaEvent_t start, stop;
+    float ms;
+
+    image_size = IMAGE_DIM * IMAGE_DIM * sizeof(uchar3);
+    image_output_size = IMAGE_DIM * IMAGE_DIM * sizeof(uchar);
+
+    // create timers
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // allocate memory on the GPU for the output image
+    cudaMalloc((void **)&d_image, image_size);
+    cudaMalloc((void **)&d_image_output, image_output_size);
+    checkCUDAError("CUDA malloc");
+
+    // allocate and load host image
+    h_image = (uchar3 *)malloc(image_size);
+    h_image_output = (uchar *)malloc(image_output_size);
+    input_image_file("input.ppm", h_image);
+
+    // copy image to device memory
+    cudaMemcpy(d_image, h_image, image_size, cudaMemcpyHostToDevice);
+    checkCUDAError("CUDA memcpy to device");
+
+    // cuda layout and execution
+    dim3 blocksPerGrid(IMAGE_DIM / 16, IMAGE_DIM / 16);
+    dim3 threadsPerBlock(16, 16);
+
+    // normal version
+    cudaEventRecord(start, 0);
+    image_to_grayscale<<<blocksPerGrid, threadsPerBlock>>>(d_image,
+                                                           d_image_output);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&ms, start, stop);
+    checkCUDAError("kernel normal");
+
+    // copy the image back from the GPU for output to file
+    cudaMemcpy(h_image_output, d_image_output, image_output_size,
+               cudaMemcpyDeviceToHost);
+    checkCUDAError("CUDA memcpy from device");
+
+    // output timings
+    printf("Execution time:");
+    printf("\t%f\n", ms);
+
+    // output image
+    output_image_file(h_image_output);
+
+    // cleanup
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaFree(d_image);
+    cudaFree(d_image_output);
+    free(h_image);
+    free(h_image_output);
+
+    return 0;
+}
+
+void output_image_file(uchar *image) {
+    FILE *f;  // output file handle
+
+    // open the output file and write header info for PPM filetype
+    const char *input_file = "output.ppm";
+    f = fopen(input_file, "wb");
+    if (f == NULL) {
+        fprintf(stderr, "Error opening 'output.ppm' output file\n");
+        exit(1);
+    }
+    fprintf(f, "P5\n");
+    fprintf(f, "# CS 629/729 Lab 05 Task01\n");
+    fprintf(f, "%d %d\n%d\n", IMAGE_DIM, IMAGE_DIM, 255);
+    for (int x = 0; x < IMAGE_DIM; x++) {
+        for (int y = 0; y < IMAGE_DIM; y++) {
+            int i = x + y * IMAGE_DIM;
+            fwrite(&image[i], sizeof(unsigned char), 1,
+                   f);  // only write garyscale
+        }
+    }
+
+    fclose(f);
+}
+
+void input_image_file(char *filename, uchar3 *image) {
+    FILE *f;  // input file handle
+    char temp[256];
+    unsigned int x, y, s;
+
+    // open the input file and write header info for PPM filetype
+    f = fopen("input.ppm", "rb");
+    if (f == NULL) {
+        fprintf(stderr, "Error opening 'input.ppm' input file\n");
+        exit(1);
+    }
+    fscanf(f, "%s\n", temp);
+    fscanf(f, "%d %d\n", &x, &y);
+    fscanf(f, "%d\n", &s);
+    if ((x != y) && (x != IMAGE_DIM)) {
+        fprintf(stderr, "Error: Input image file has wrong fixed dimensions\n");
+        exit(1);
+    }
+
+    for (int x = 0; x < IMAGE_DIM; x++) {
+        for (int y = 0; y < IMAGE_DIM; y++) {
+            int i = x + y * IMAGE_DIM;
+            fread(&image[i], sizeof(unsigned char), 3, f);
+        }
+    }
+
+    fclose(f);
+}
+
+void checkCUDAError(const char *msg) {
+    cudaError_t err = cudaGetLastError();
+    if (cudaSuccess != err) {
+        fprintf(stderr, "CUDA ERROR: %s: %s.\n", msg, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
